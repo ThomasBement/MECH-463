cd6152d83bb0f496e1c4c807fca4202a7a838926
ThomasBement <46405515+ThomasBement@users.noreply.github.com>
2021-11-15 23:20:59 -0800

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from csv import reader
+from scipy.optimize import curve_fit
 
 """
 CONSTANTS
@@ -27,6 +28,8 @@ g = 9.81
 """
 READ IN DATA
 """
+section_lis = ['6.1', '6.2', '6.3', '6.4', '6.5.1', '6.5.2', '6.6', '6.7']
+
 # Function to read specific CSV format
 def read_csv(path):
     ans = {}
@@ -43,10 +46,25 @@ def read_csv(path):
                     ans[headers[j]].append(float(row[j]))
     return ans, path.split(' ')[0].split('\\')[-1]
 
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
+
+def coeff_determ(y, y_fit):
+    y = np.array(y)
+    y_fit = np.array(y_fit)
+    fitted = []
+    for i in range(len(y)):
+        fitted.append(find_nearest(y_fit, y[i]))
+    ss_res = np.sum((y - fitted) ** 2)
+    ss_tot = np.sum((y - np.mean(y)) ** 2)
+    return 1 - (ss_res / ss_tot)
+
 # Generating file names to read
 fil_list = []
-for i in range(1, 8):
-    fil_list.append(r'.\DATA\6.%i Export.csv' %i)
+for i in range(len(section_lis)):
+    fil_list.append(r'.\DATA\%s Export.csv' %section_lis[i])
 
 # Reading all data from all files into one dictonary
 all_dat = {}
@@ -63,19 +81,30 @@ for section in all_dat:
 """
 SECTION 6.2
 """
-section_lis = ['6.1', '6.2', '6.3', '6.4', '6.5', '6.6', '6.7']
+
+def lin_model(x, a, b):
+    return (a*x) + b
 
 plt_dat = {'D': [], 'Mean': [], 'STD_Err': []}
 section = '6.2'
 for test in all_dat[section]:
-    plt_dat['D'].append(float(test.split('=')[-1]))
+    plt_dat['D'].append(float(test.split('=')[-1])/100)
     plt_dat['Mean'].append(np.mean(all_dat[section][test]))
     plt_dat['STD_Err'].append(np.std(all_dat[section][test])/np.sqrt(np.shape(all_dat[section][test])[0]))
 
-plt.errorbar(plt_dat['D'], plt_dat['Mean'], yerr=plt_dat['STD_Err'], marker='.')
+p0 = [1, 0]
+popt, pcov = curve_fit(lin_model, plt_dat['D'], plt_dat['Mean'], p0=p0, sigma=plt_dat['STD_Err'], absolute_sigma=True)
+x_fit = np.linspace(min(plt_dat['D']), max(plt_dat['D']), 256)
+y_fit = lin_model(x_fit, *popt)
+
+print(*popt)
+
+plt.errorbar(plt_dat['D'], plt_dat['Mean'], yerr=plt_dat['STD_Err'], marker='.', ls='none', label='Measured Values')
+plt.plot(x_fit, y_fit, label='Fitted Model')
 plt.title('Section 6.2: Frequency vs. String Seperation')
 plt.ylabel('Frequency of Oscillation [Hz]')
-plt.xlabel('String Seperation D [cm]')
+plt.xlabel('String Seperation D [m]')
+plt.legend()
 plt.savefig('Seperation.png', format='png', bbox_inches='tight')
 plt.show()
 plt.close()
@@ -109,21 +138,32 @@ plt.close()
 """
 SECTION 6.5
 """
-
 # From MatLab
 LR = [0.70,0.75,0.80,0.85,0.90,0.95,1.00,1.05,1.10,1.15,1.20,1.25,1.30]
-f = [0.5696,0.5687,0.5674,0.5656,0.5629,0.5588,0.5528,0.5451,0.5361,0.5265,0.5169,0.5075,0.4984]
-
+f_low = [0.5696,0.5687,0.5674,0.5656,0.5629,0.5588,0.5528,0.5451,0.5361,0.5265,0.5169,0.5075,0.4984]
+f_high = [0.6912087152,0.6688417596,0.6490423105,0.6316839905,0.616840967,0.6047892301,0.5958070192,0.5897274062,0.5858377171,0.5833422718,0.5816802556,0.5805210906,0.5796771558]
 
 plt_dat = {'L2/L1': [], 'Mean': [], 'STD_Err': []}
-section = '6.5'
+section = '6.5.1'
 for test in all_dat[section]:
     plt_dat['L2/L1'].append(float(test.split('=')[-1]))
     plt_dat['Mean'].append(np.mean(all_dat[section][test]))
     plt_dat['STD_Err'].append(np.std(all_dat[section][test])/np.sqrt(np.shape(all_dat[section][test])[0]))
 
-plt.errorbar(plt_dat['L2/L1'], plt_dat['Mean'], yerr=plt_dat['STD_Err'], marker='.', label='Measured')
-plt.plot(LR, f, label='Theoretical')
+plt.errorbar(plt_dat['L2/L1'], plt_dat['Mean'], yerr=plt_dat['STD_Err'], marker='.', label='Measured Low Frequency')
+
+plt_dat = {'L2/L1': [], 'Mean': [], 'STD_Err': []}
+section = '6.5.2'
+for test in all_dat[section]:
+    plt_dat['L2/L1'].append(float(test.split('=')[-1]))
+    plt_dat['Mean'].append(np.mean(all_dat[section][test]))
+    plt_dat['STD_Err'].append(np.std(all_dat[section][test])/np.sqrt(np.shape(all_dat[section][test])[0]))
+
+print(coeff_determ(f_high, plt_dat['Mean']))
+plt.errorbar(plt_dat['L2/L1'], plt_dat['Mean'], yerr=plt_dat['STD_Err'], marker='.', label='Measured High Frequency')
+
+plt.plot(LR, f_low, label='Theoretical Low Frequency')
+plt.plot(LR, f_high, label='Theoretical High Frequency')
 plt.legend()
 plt.title('Section 6.5: Frequency vs. Length Ratio')
 plt.ylabel('Frequency of Oscillation [Hz]')
